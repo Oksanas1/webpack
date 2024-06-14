fd17fad48408b455334c06dc2f8f9b5ae9d3138b
Oksana <benefit989@gmail.com>
2024-06-14 23:52:58 +0300

    first commit

--- lesson1/clw2/dist/scripts/storage.js
+++
@@ -1,5 +0,0 @@
-export const getLocalStorageData = key => JSON.parse(localStorage.getItem(key)) || [];
-
-export const setToLocalStorageData = (key, value) => {
-  localStorage.setItem(key, JSON.stringify(value));
-};
