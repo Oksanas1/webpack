600204dcdb75be1067ac731df2aae639282f5a07
Oksana <benefit989@gmail.com>
2024-06-16 16:20:43 +0300

    fix watch:scripts

--- lesson5/clw1/webpack.config.js
+++ lesson5/clw1/webpack.config.js
@@ -2,9 +2,8 @@ const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-    target: "node",
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
   }
